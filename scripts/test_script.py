added models; fix normalization and noise levels

scripts/test_script.py
@@ -35,15 +35,25 @@ args = parser.parse_args()
 cfg_test = load_config(args.config_test)
 cfg_data = load_config(args.config_data)
 
+noise_std = cfg_test["testing"]["noise_std"]
+if not (isinstance(noise_std, float) or isinstance(noise_std, int)):
+    raise ValueError(f"noise standard deviation should be float or integer, got {noise_std}")
+
+n_k1 = cfg_test["testing"]["n_k1"]
+if not (isinstance(n_k1, float) or isinstance(n_k1, int)):
+    raise ValueError(f"A single number of n_k1 samples (`float` or `int`) should be "
+                     f"given for model testing; got {type(n_k1)}")
+
 data_dir = cfg_data["data_dir"]
 split_dir = cfg_data["split_dir"]
 
 batch_size = cfg_test["testing"]["batch_size"]
 
+n_test = cfg_test["testing"]["n_test"]
 test_files = read_split_file(
     data_dir=data_dir,
     split_file=split_dir + "fastmri_test.txt",
-)[:50]
+)[:n_test]
 
 test_image_data = mrpro.phantoms.FastMRIImageDataset(
     path=test_files,
@@ -54,8 +64,8 @@ base_seed = 42
 
 test_data = LowFieldMRDataset(
     image_dataset=test_image_data,
-    noise_variance=cfg_test["testing"]["noise_variance"],
-    n_k1=cfg_test["testing"]["n_k1"],
+    noise_variance=noise_std ** 2,
+    n_k1=n_k1,
     base_seed=base_seed,
 )
 
@@ -63,25 +73,31 @@ test_loader = torch.utils.data.DataLoader(
     test_data, batch_size=batch_size, shuffle=False
 )
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if cfg_test["model"]["name"] == "modl":
+    n_layers = cfg_test["model"]["cnn_block"]["n_layers"]
+    n_filters = cfg_test["model"]["cnn_block"]["n_filters"]
     cnn_block = MoDLBlock(n_layers=cfg_test["model"]["cnn_block"]["n_layers"],
                           n_ch_in=cfg_test["model"]["cnn_block"]["n_ch_in"],
                           n_ch_out=cfg_test["model"]["cnn_block"]["n_ch_out"],
-                          n_filters=cfg_test["model"]["cnn_block"]["n_filters"]
+                          n_filters=n_filters
                           )
     model = MoDL(cnn_block=cnn_block, n_iterations=cfg_test["model"]["n_iterations"]).to(device)
+
+    hyperparameters_identification = f"n_layers{n_layers}_n_filters{n_filters}"
     
 elif cfg_test["model"]["name"] in ["cdl", "tv"]:
+    n_enc_stages = cfg_test["model"]["cnn_block"]["n_enc_stages"]
+    n_convs_per_stage = cfg_test["model"]["cnn_block"]["n_convs_per_stage"]
+    n_filters = cfg_test["model"]["cnn_block"]["n_filters"]
     cnn_block = UNet(
         dim=2,
-        n_ch_in=cfg_test["cnn_block"]["n_ch_in"],
-        n_ch_out=cfg_test["cnn_block"]["n_ch_out"],
-        n_enc_stages=cfg_test["cnn_block"]["n_enc_stages"],
-        n_convs_per_stage=cfg_test["cnn_block"]["n_convs_per_stage"],
-        n_filters=cfg_test["cnn_block"]["n_filters"],
+        n_ch_in=cfg_test["model"]["cnn_block"]["n_ch_in"],
+        n_ch_out=cfg_test["model"]["cnn_block"]["n_ch_out"],
+        n_enc_stages=n_enc_stages,
+        n_convs_per_stage=n_convs_per_stage,
+        n_filters=n_filters,
         kernel_size=3,
         pooling_kernel_size=2,
         bias=False,
@@ -93,21 +109,40 @@ elif cfg_test["model"]["name"] in ["cdl", "tv"]:
             n_iterations=cfg_test["model"]["n_iterations"]
         ).to(device)
         
+        hyperparameters_identification = f"n_enc_stages{n_enc_stages}_"\
+            f"n_convs_per_stage{n_convs_per_stage}_n_filters{n_filters}"
+        
     else:
+        n_conv_dictionary_filters = cfg_test["model"]["conv_dictionary"]["n_conv_kernel_filters"]
+        kernel_size = cfg_test["model"]["conv_dictionary"]["n_conv_kernel_size"]
+        sparsity_param = cfg_test["model"]["conv_dictionary"]["sparsity_param"]
+        lowpass_param = cfg_test["model"]["conv_dictionary"]["lowpass_param"]
+        
+        conv_dictionary_kernel_dir = cfg_data["conv_dictionary_dir"]
+        kernel_fname = Path(f"K{n_conv_dictionary_filters}_k{kernel_size}x{kernel_size}_"\
+                        f"sparsity_param{str(sparsity_param).replace(".","_")}_"\
+                        f"lowpass_param{str(lowpass_param).replace(".","_")}.pt")
+        conv_dictionary_kernel = torch.load(Path(conv_dictionary_kernel_dir) / kernel_fname)
+        
         parameter_map_network = ConvSynthesisParameterMapNetwork2D(cnn_block = cnn_block)
         model = SpatiallyAdaptiveConvSynthesisNet2D(
+            kernel = conv_dictionary_kernel,
             parameter_map_network=parameter_map_network,
             n_iterations=cfg_test["model"]["n_iterations"]
         ).to(device)
+        
+        hyperparameters_identification = f"n_enc_stages{n_enc_stages}_"\
+            f"n_convs_per_stage{n_convs_per_stage}_n_filters{n_filters}_"\
+            f"n_conv_dictionary_filters{n_conv_dictionary_filters}_conv_kernel_size{kernel_size}"
 else:
     raise ValueError(f"Model name should be either 'modl', 'cdl', or 'tv', but got{cfg_test["model"]["name"]}")
 
 
 run_dir_name = Path(f"{cfg_test["model"]["name"]}")
-run_dir = Path("runs") / run_dir_name # Path.joinpath(run_dir_name, datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-
+run_dir = Path("runs") / run_dir_name  / hyperparameters_identification
+run_dir.mkdir(parents=True, exist_ok=True)
 checkpoint = torch.load(run_dir / "model.pt", map_location=device)
 model.load_state_dict(checkpoint["model_state_dict"])
 
-metrics_fname = f"test_metrics_{cfg_test["testing"]["noise_variance"]}"
+metrics_fname = f"test_metrics_{cfg_test["testing"]["noise_std"]}"
 test_model(model, test_loader, device, run_dir, metrics_fname)

scripts/training_script.py
@@ -141,7 +141,7 @@ elif cfg_training["model"]["name"] in ["cdl", "tv"]:
                 "weight_decay": cfg_training["training"]["weight_decay"],
             },
             {
-                "params": model.parameter_map_network._global_scaling, 
+                "params": [model.parameter_map_network._global_scaling], 
                 "lr": cfg_training["training"]["learning_rate_global_scaling"]
             },
         ]
@@ -157,8 +157,8 @@ elif cfg_training["model"]["name"] in ["cdl", "tv"]:
         
         conv_dictionary_kernel_dir = cfg_data["conv_dictionary_dir"]
         kernel_fname = Path(f"K{n_conv_dictionary_filters}_k{kernel_size}x{kernel_size}_"\
-                        f"sparsity_param{str(sparsity_param).replace(".","_")}_"\
-                        f"lowpass_param{str(lowpass_param).replace(".","_")}.pt")
+                        f"sparsity_param{str(sparsity_param).replace('.','_')}_"\
+                        f"lowpass_param{str(lowpass_param).replace('.','_')}.pt")
         conv_dictionary_kernel = torch.load(Path(conv_dictionary_kernel_dir) / kernel_fname)
         
         parameter_map_network = ConvSynthesisParameterMapNetwork2D(cnn_block = cnn_block)
@@ -170,13 +170,13 @@ elif cfg_training["model"]["name"] in ["cdl", "tv"]:
         
         params_list = [
             {
-                "params": model.parameter_map_network.cnn_block.parameters(),
+                "params": list(model.parameter_map_network.cnn_block.parameters()),
                 "lr": cfg_training["training"]["learning_rate"],
                 "weight_decay": cfg_training["training"]["weight_decay"],
             },
             {
-                "params": model.parameter_map_network._global_scaling, 
-                "lr": cfg_training["training"]["learning_rate_global_scaling"]
+                "params": [model._low_pass_filtering_parameter], 
+                "lr": cfg_training["training"]["learning_rate_low_pass_param"]
             },
         ]
         
@@ -184,28 +184,24 @@ elif cfg_training["model"]["name"] in ["cdl", "tv"]:
             f"n_convs_per_stage{n_convs_per_stage}_n_filters{n_filters}_"\
             f"n_conv_dictionary_filters{n_conv_dictionary_filters}_conv_kernel_size{kernel_size}"
 else:
-    raise ValueError(f"Model name should be either 'modl', 'cdl', or 'tv', but got{cfg_training["model"]["name"]}")
+    raise ValueError(f"Model name should be either 'modl', 'cdl', or 'tv', but got{cfg_training['model']['name']}")
 
 
 optimizer = torch.optim.Adam(params=params_list)
-
-# collect all hyperparameters defining the experiment
-config_file = {
-        name: value
-        for name, value in locals().items()
-        if isinstance(value, (int, float, tuple, str, bool))
-    }
-
 
 run_dir_name = Path(f"{cfg_training["model"]["name"]}")
 run_dir = Path("runs") / run_dir_name  / hyperparameters_identification
 run_dir.mkdir(parents=True, exist_ok=True)
 
 experiment_name = f"{cfg_training["model"]["name"]}_{hyperparameters_identification}"
-wandb_config = {"project": "AdaptiveL1",
-                "experiment_name": experiment_name,
-                "config": config_file,
+
+# collect all hyperparameters defining the experiment
+config = {
+        name: value
+        for name, value in locals().items()
+        if isinstance(value, (int, float, tuple, str, bool))
     }
+
 model = train_model(
     model = model, 
     training_loader=training_loader, 
@@ -215,5 +211,5 @@ model = train_model(
     device = device,
     n_epochs=cfg_training["training"]["n_epochs"],
     run_dir=run_dir,
-    wandb_config=wandb_config
+    config=config
 )

src/adaptive_l1/data/data_classes.py
@@ -73,6 +73,8 @@ class LowFieldMRDataset(torch.utils.data.Dataset):
         kdata_noisy, adjoint = generate_kspace_data_and_adjoint(
             image, mask_operator, noise_variance, generator
         )
+        
+        kdata_noisy, adjoint, image = normalize_kspace_data_and_images(kdata_noisy, adjoint, image)
 
         return {
             "kdata": kdata_noisy.squeeze(0), #remove unnecessary batch dimension
@@ -80,7 +82,6 @@ class LowFieldMRDataset(torch.utils.data.Dataset):
             "mask": mask_operator.mask.squeeze(0),
             "target": image.squeeze(0),
         }
-
 
 def sample_parameter(spec: int | float | tuple | list, generator: torch.Generator):
     """Given a parameter specification, sample a value according to the specification.
@@ -187,6 +188,20 @@ def add_kdata_noise(
 
     return kdata
 
+def normalize_kspace_data_and_images(
+    kdata: torch.Tensor,
+    adjoint: torch.Tensor | None,
+    target: torch.Tensor | None,
+) -> tuple[torch.Tensor, torch.Tensor | None, torch.Tensor | None]:
+    """Normalize k-space data and (possibly) adjoint and target images."""
+    factor = 1.0 / kdata.abs().std()
+    kdata *= factor
+    if adjoint is not None:
+        adjoint *= factor
+    if target is not None:
+        target *= factor
+    return kdata, adjoint, target
+
 def generate_kspace_data_and_adjoint(
     image: torch.Tensor,
     mask_operator: mrpro.operators.CartesianMaskingOp,
@@ -208,17 +223,19 @@ def generate_kspace_data_and_adjoint(
     fourier_operator = mrpro.operators.FastFourierOp(dim=(-2, -1)).to(image.device)
 
     (kdata_full,) = fourier_operator(image)
-
-    n_samples = torch.prod(torch.tensor(kdata_full.shape[1:]))
-    noise_variance_effective = noise_variance * (kdata_full.abs().pow(2).sum(
-        dim=(-2, -1), keepdim=True)
-     / n_samples).pow(0.5)
+    
+    signal_rms = kdata_full.abs().pow(2).mean(
+        dim=(-2, -1), keepdim=True
+    ).sqrt()
+    
+    noise_variance = noise_variance * signal_rms.square()
     
     (kdata,) = mask_operator(kdata_full)
 
     kdata_noisy = add_kdata_noise(
-        kdata, mask_operator, noise_variance_effective, generator
+        kdata, mask_operator, noise_variance, generator
     )
+    
     forward_operator = mask_operator @ fourier_operator
 
     (adjoint,) = forward_operator.H(kdata_noisy)

src/adaptive_l1/training/trainer.py
@@ -1,7 +1,21 @@
 import mrpro
 import torch
-
 from tqdm import tqdm
+import itertools
+
+
+def forward_pass(model, batch, device):
+
+    kdata = batch["kdata"].to(device)
+    adjoint = batch["adjoint"].to(device)
+    mask = batch["mask"].to(device)
+    target = batch["target"].to(device)
+
+    mask_operator = mrpro.operators.CartesianMaskingOp(mask)
+
+    recon = model(adjoint, kdata, mask_operator)
+
+    return recon, target
 
 
 def train_model(
@@ -56,19 +70,12 @@ def train_model(
             leave=False,
             disable=False,
         ):
-            kdata = batch["kdata"].to(device)
-            adjoint = batch["adjoint"].to(device)
-            mask = batch["mask"].to(device)
-            target = batch["target"].to(device)
-
-            mask_operator = mrpro.operators.CartesianMaskingOp(mask)
-
-            optimizer.zero_grad()
-            recon = model(adjoint, kdata, mask_operator)
+            recon, target = forward_pass(model, batch, device)
             loss = loss_function(
                 torch.view_as_real(recon),
                 torch.view_as_real(target),
             )
+            optimizer.zero_grad(set_to_none=True)
             loss.backward()
             optimizer.step()
 
@@ -84,14 +91,7 @@ def train_model(
                 leave=False,
                 disable=False,
             ):
-                kdata = batch["kdata"].to(device)
-                adjoint = batch["adjoint"].to(device)
-                mask = batch["mask"].to(device)
-                target = batch["target"].to(device)
-
-                mask_operator = mrpro.operators.CartesianMaskingOp(mask)
-
-                recon = model(adjoint, kdata, mask_operator)
+                recon, target = forward_pass(model, batch, device)
                 loss = loss_function(
                     torch.view_as_real(recon),
                     torch.view_as_real(target),
@@ -104,12 +104,17 @@ def train_model(
             validation_loss = validation_loss_sum / validation_n_samples
 
         if use_wandb:
+            sample_id = 5  # just picked for demonstration purposes
+            batch = next(itertools.islice(validation_loader, sample_id, sample_id + 1))
+
+            recon, target = forward_pass(model, batch, device)
+
             wandb.log({"validation-loss": validation_loss}, step=epoch + 1)
             fig, ax = plt.subplots(1, 3, figsize=(1 * 15, 3 * 15))
             recons_list = [
-                adjoint.detach().cpu(),
+                batch["adjoint"].detach().cpu(),
                 recon.detach().cpu(),
-                target.detach().cpu(),
+                batch["target"].detach().cpu(),
             ]
             titles_list = ["adjoint", "recon", "target"]
             for k, (recon_, title) in enumerate(zip(recons_list, titles_list)):